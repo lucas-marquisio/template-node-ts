f297d63ed7b47e15a5c8119b391f3987aa96da4c
Lucas Marquisio <contato.lucasmarquisio@gmail.com>
2023-07-04 19:02:29 -0300

    refactor: port application

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,9 @@
 import express, { Express } from 'express'
 import setupRoutes from './set-routes'
+import { PORT } from 'utils/variables'
 
 export const app: Express = express()
 
 setupRoutes(app)
 
-app.listen(4000)
+app.listen(PORT)
